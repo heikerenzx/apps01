7f3528321c6c2fe28f1efc2570822c9f2bfbc87f
heikerenzx <37391969+heikerenzx@users.noreply.github.com>
2018-03-15 11:49:28 +0800

    Create Model.class.php

---
+++ Model/Model.class.php
@@ -0,0 +1,22 @@
+<?php 
+	class db
+	{
+		/**
+		 * @varname1 主机名
+		 * @varname2 用户名
+		 * @varname3 数据库密码
+		 * @varname4 数据库名
+		 * @varname5 编码格式
+		 */
+		private $hostname;
+		private $root;
+		private $password;
+		private $dbname;
+		private $charset;
+		
+		public function __construct()
+		{
+			
+		}
+	}
+?>
